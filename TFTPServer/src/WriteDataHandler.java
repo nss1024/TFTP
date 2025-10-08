b3b6c35425d053f16601a42c7b46cbbe90c017f0
nss1024 <32052333+nss1024@users.noreply.github.com>
2025-10-08 06:50:31 +0100

    refactored WriteDataHandler to use utility methods from TFTPUtils class, removed unnecessary local methods

--- TFTPServer/src/WriteDataHandler.java
+++ TFTPServer/src/WriteDataHandler.java
@@ -1,4 +1,3 @@
-import configLoader.ConfigLoader;
 import encoding.NetAsciiDecoder;
 import watchdog.DataStore;
 import watchdog.WatchDogMonitoredSession;
@@ -8,8 +7,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.*;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,6 +19,7 @@ public class WriteDataHandler implements Runnable, WatchDogMonitoredSession {
     String ip = "";
     int destPort = 0;
     int localPort = 8888;
+    private final int SOCKET_CREATE_TIMEOUT=60;
     DatagramSocket ds = null;
     DatagramPacket dp = null;
     short blockNo=0;
@@ -48,22 +46,18 @@ public class WriteDataHandler implements Runnable, WatchDogMonitoredSession {
     public void run() {
         sessionThread=Thread.currentThread();
         DataStore.addSessionToDataStore(this);
-        createDatagramSocket();
+        ds=TFTPUtils.getDatagramSocket(localPort,SOCKET_CREATE_TIMEOUT);
         String fileName="";
         String mode="";
         FileOutputStream fos=null;
         try {//handle file setup
             //get filename from datagram packet
-            fileName = getText(data, 2, 0);
+            fileName = TFTPUtils.getText(data, 2, 0);
             //try to create file
             fos = new FileOutputStream(new File(PATH + "/" + fileName));
         } catch (FileNotFoundException e) {
             logger.log(Level.WARNING, "Error creating file! ");
-            try {
-               sendError(ds,2);
-            } catch (IOException ex) {
-                logger.log(Level.WARNING, "Failed to send error message to client!" + ip);
-            }
+               TFTPUtils.sendError(ds,2,TFTPUtils.getError(2),ip,destPort);
             throw new RuntimeException(e);
         } catch (IllegalArgumentException e) {
             logger.log(Level.WARNING, "Error parsing byte array! ");
@@ -72,15 +66,13 @@ public class WriteDataHandler implements Runnable, WatchDogMonitoredSession {
 
         //check mode; Supported mode = octet, NetAscii
         try {
-            mode = getText(data, 2, 1).toLowerCase();//get mode from datagram packet
+            mode = TFTPUtils.getText(data, 2, 1).toLowerCase();//get mode from datagram packet
             if (!mode.equals("octet") && !mode.equals("netascii")) {
-                try {
-                    sendError(ds, 0);
+
+                    TFTPUtils.sendError(ds,0,TFTPUtils.getError(0),ip,destPort);
                     logger.log(Level.WARNING, "Only octet or netascii mode supported, received request for " + mode + " mode");
                     throw new RuntimeException();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+
             }
         } catch (Exception e) {
             logger.log(Level.SEVERE,"Unable to verify mode!");
@@ -88,11 +80,8 @@ public class WriteDataHandler implements Runnable, WatchDogMonitoredSession {
         }
 
             //else reply with ACK and start reading loop
-            try {
-                sendACK(ds,(short)0);
-            } catch (IOException ex) {
-                logger.log(Level.WARNING, "Failed to send error message to client!" + ip);
-            }
+                TFTPUtils.sendACK(ds,(short)0,ip,destPort);
+
 
 
             while(running){//if all is well, start listening for inbound packets.
@@ -102,25 +91,25 @@ public class WriteDataHandler implements Runnable, WatchDogMonitoredSession {
                     buffer = Arrays.copyOf(dp.getData(),dp.getData().length);//get data from datagram packet, copying just in case, no other packet should arrive without us sending an ack
                     if(buffer.length<516){running=false;}//last data packed received
 
-                    if(getOpCode(buffer)!=3){//only accept data packets, if anything else, log it and exit loop
-                        logger.log(Level.WARNING,"Non data op code received"+getOpCode(buffer));
-                        sendError(ds,5);
+                    if(TFTPUtils.getOpCode(buffer)!=3){//only accept data packets, if anything else, log it and exit loop
+                        logger.log(Level.WARNING,"Non data op code received"+TFTPUtils.getOpCode(buffer));
+                        TFTPUtils.sendError(ds,5,TFTPUtils.getError(5),ip,destPort);
                         break;
                     }
 
                     //check for duplicate data packets
                     //TODO: make duplicate counter configurable
-                    if(getBlockNo(buffer)==blockNo){
+                    if(TFTPUtils.getBlockNo(buffer)==blockNo){
                         logger.log(Level.WARNING,"Duplicate packet detected");
                         if(duplicatePacketCounter==3){
-                            sendError(ds,4);
+                            TFTPUtils.sendError(ds,4,TFTPUtils.getError(4),ip,destPort);
                             break;
                         }
                         duplicatePacketCounter++;
                     }
-                    blockNo=getBlockNo(buffer);//update block number
+                    blockNo=TFTPUtils.getBlockNo(buffer);//update block number
                     dataBuffer.add(Arrays.copyOfRange(buffer,4,buffer.length));//save data to Arraylist
-                    sendACK(ds,getBlockNo(buffer));
+                    TFTPUtils.sendACK(ds,TFTPUtils.getBlockNo(buffer),ip,blockNo);
                 } catch (IOException ex) {
                     logger.log(Level.WARNING,"Error processing data packet!");
                 }
@@ -154,136 +143,6 @@ public class WriteDataHandler implements Runnable, WatchDogMonitoredSession {
 
 
     }
-
-        private short getBlockNo ( byte[] b){
-            return getShort(b, 2);
-        }
-
-        private short getOpCode ( byte[] b){
-            return getShort(b, 0);
-        }
-
-        private String getText ( byte[] b, int offset, int mode) throws IllegalArgumentException {
-            //Mode 1 = get the second block of text after encountering the first 0 terminator, this is useful when getting the mode
-            if (offset >= b.length) {
-                throw new IllegalArgumentException("Offset out of bounds.");
-            }
-
-            int start = offset;
-            int end;
-
-            // Step 1: Find the first null terminator
-            end = start;
-            while (end < b.length && b[end] != 0) {
-                end++;
-            }
-
-            if (end == b.length) {
-                throw new IllegalArgumentException("Missing null terminator after first string.");
-            }
-
-            if (mode == 0) {
-                // Extract filename
-                return new String(b, start, end - start, StandardCharsets.UTF_8);
-            }
-
-            // Step 2: Extract the second string (mode)
-            start = end + 1; // move past first null terminator
-            if (start >= b.length) {
-                throw new IllegalArgumentException("No second string found.");
-            }
-
-            end = start;
-            while (end < b.length && b[end] != 0) {
-                end++;
-            }
-
-            if (end == b.length) {
-                throw new IllegalArgumentException("Missing null terminator after second string.");
-            }
-
-            return new String(b, start, end - start, StandardCharsets.UTF_8);
-        }
-
-        private short getShort ( byte[] b, int startIndex){
-            short result = (short) 0;
-            try {
-                result = (short) (((b[startIndex] & 0XFF) << 8) | (b[startIndex + 1] & 0XFF));
-            } catch (Exception e) {
-                logger.log(Level.WARNING, "Error getting short from byte array! ");
-            }
-            return result;
-        }
-
-        private String getError ( int code){
-            switch (code) {
-                case 0:
-                    return "Not defined";
-
-                case 1:
-                    return "File not found";
-
-                case 2:
-                    return "Access violation";
-
-                case 3:
-                    return "Disk full or allocation exceeded";
-
-                case 4:
-                    return "Illegal TFTP operation";
-
-                case 5:
-                    return "Unknown transfer ID";
-
-                case 6:
-                    return "File already exists";
-
-                case 7:
-                    return "No such user";
-
-                default:
-                    return "Unknown error";
-
-            }
-        }
-
-        private void sendACK(DatagramSocket ds, short blockNo) throws IOException{
-            ByteBuffer bb = ByteBuffer.allocate(4);
-            bb.putShort((short) 4);//op code for ACK
-            bb.putShort((short) blockNo);//Block number of 0 to indicate transfer can be started
-
-            try {
-                ds.send(new DatagramPacket(bb.array(), bb.array().length, InetAddress.getByName(ip), destPort));
-            } catch (IOException ex) {
-                logger.log(Level.WARNING, "Failed to send error message to client!" + ip);
-            }
-        }
-
-        private void sendError(DatagramSocket ds, int errorCode) throws IOException{
-
-            ByteBuffer bb = ByteBuffer.allocate(5 + getError(errorCode).length());
-            bb.putShort((short) 5);//op code indicating error
-            bb.putShort((short) errorCode);//Specific error code
-            bb.put(getError(errorCode).getBytes(StandardCharsets.UTF_8));//error message
-            bb.put((byte) 0);//"0" terminator
-
-            try {
-                ds.send(new DatagramPacket(bb.array(), bb.array().length, InetAddress.getByName(ip), destPort));
-            } catch (IOException ex) {
-                logger.log(Level.WARNING, "Failed to send error message to client!" + ip);
-            }
-
-        }
-
-        private void createDatagramSocket(){
-            try {
-                ds = new DatagramSocket(localPort);//create local datagram socket
-            } catch (SocketException e) {
-                logger.log(Level.WARNING,"Error initializing datagram socket!");
-                throw new RuntimeException();
-            }
-        }
-
         public boolean isAlive(){
         return running;
         }
